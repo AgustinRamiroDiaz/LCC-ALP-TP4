c17350f3a17b40ca89f54c57df795cf20402abe1
Juan Cruz De La Torre <delatorrejuanchi@gmail.com>
2020-11-25 13:51:58 -0300

    refactor, borrar trailing spaces

--- src/Eval1.hs
+++ src/Eval1.hs
@@ -28,7 +28,7 @@ initEnv = M.empty
 newtype State a = State { runState :: Env -> Pair a Env }
 
 instance Monad State where
-  return x = State (\s -> (x :!: s))
+  return x = State (x :!:)
   m >>= f = State (\s -> let (v :!: s') = runState m s in runState (f v) s')
 
 -- Para calmar al GHC
@@ -40,9 +40,9 @@ instance Applicative State where
   (<*>) = ap
 
 instance MonadState State where
-  lookfor v = State (\s -> (lookfor' v s :!: s))
+  lookfor v = State (\s -> lookfor' v s :!: s)
     where lookfor' v s = fromJust $ M.lookup v s
-  update v i = State (\s -> (() :!: update' v i s)) where update' = M.insert
+  update v i = State (\s -> () :!: update' v i s) where update' = M.insert
 
 -- Ejercicio 1.b: Implementar el evaluador utilizando la monada State
 
@@ -58,9 +58,9 @@ stepCommStar c    = stepComm c >>= \c' -> stepCommStar c'
 -- Evalua un paso de un comando
 stepComm :: MonadState m => Comm -> m Comm
 stepComm Skip = return Skip
-stepComm (Let v e) = 
+stepComm (Let v e) =
     do
-        r <- evalExp e 
+        r <- evalExp e
         update v r
         return Skip
 stepComm (Seq Skip c2) = stepComm c2
@@ -77,58 +77,57 @@ evalExp :: MonadState m => Exp a -> m a
 evalExp (Const x) = return x
 evalExp (Var x) = lookfor x
 evalExp (UMinus x) = evalExp x >>= \y -> return (-y)
-evalExp (Plus x y) = 
-    do 
+evalExp (Plus x y) =
+    do
         x' <- evalExp x
         y' <- evalExp y
         return (x' + y')
-
-evalExp (Minus x y) = 
-    do 
+evalExp (Minus x y) =
+    do
         x' <- evalExp x
         y' <- evalExp y
         return (x' - y')
-evalExp (Times x y) = 
-    do 
+evalExp (Times x y) =
+    do
         x' <- evalExp x
         y' <- evalExp y
         return (x' * y')
-evalExp (Div x y) = 
-    do 
+evalExp (Div x y) =
+    do
         x' <- evalExp x
         y' <- evalExp y
         return (x' `div` y')
 -- Bool
 evalExp BTrue = return True
 evalExp BFalse = return False
-evalExp (Lt x y) = 
-    do 
+evalExp (Lt x y) =
+    do
         x' <- evalExp x
         y' <- evalExp y
         return (x' < y')
-evalExp (Gt x y) = 
-    do 
+evalExp (Gt x y) =
+    do
         x' <- evalExp x
         y' <- evalExp y
         return (x' > y')
-evalExp (And x y) = 
-    do 
+evalExp (And x y) =
+    do
         x' <- evalExp x
         y' <- evalExp y
         return (x' && y')
-evalExp (Or x y) = 
-    do 
+evalExp (Or x y) =
+    do
         x' <- evalExp x
         y' <- evalExp y
         return (x' || y')
 evalExp (Not x) = evalExp x >>= \y -> return (not y)
-evalExp (Eq x y) = 
-    do 
+evalExp (Eq x y) =
+    do
         x' <- evalExp x
         y' <- evalExp y
         return (x' == y')
-evalExp (NEq x y) = 
-    do 
+evalExp (NEq x y) =
+    do
         x' <- evalExp x
         y' <- evalExp y
         return (x' /= y')
@@ -137,6 +136,4 @@ evalExp (EAssgn v exp) =
         r <- evalExp exp
         update v r
         return r
-evalExp (ESeq e1 e2) = 
-    evalExp e1 >>= \r -> evalExp e2
-
+evalExp (ESeq e1 e2) = evalExp e1 >> evalExp e2
